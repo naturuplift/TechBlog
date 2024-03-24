5a72140168a398a42d76293e4358f591e3f056e9
Arnaldo <arnaldosepulveda@gmail.com>
2024-03-24 12:39:07 -0300

    add squema.sql

--- db/squema.sql
+++ db/squema.sql
@@ -1,2 +1,5 @@
+-- Drop the existing database if it exists
 DROP DATABASE IF EXISTS tech_blog_app_db;
+
+-- Create a new database
 CREATE DATABASE tech_blog_app_db;
---
+++ seeds/index.js
@@ -0,0 +1,5 @@
+// Import Sequelize connection and all seed files
+const sequelize = require('../config/connection');
+
+
+
